511dad827e534483b0f0156d9e7c15d59ccd1e69
rnkwilliams <rnkwilliams07@gmail.com>
2020-11-27 14:38:19 -0600

    create publisher migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_27_202352) do
+ActiveRecord::Schema.define(version: 2020_11_27_203616) do
+
+  create_table "publishers", force: :cascade do |t|
+    t.string "name"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_publishers_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
@@ -19,4 +27,5 @@ ActiveRecord::Schema.define(version: 2020_11_27_202352) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "publishers", "users"
 end
